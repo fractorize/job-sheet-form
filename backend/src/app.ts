465f77e545ab037f12946cac2eb4561e7960bfec
safwan-ms <safwan.anasms@gmail.com>
2025-09-13 12:49:25 +0530

    chore(server): Initialize backend with Express, Mongoose config, and environment setup

---
+++ backend/src/app.ts
@@ -0,0 +1,27 @@
+// packages
+import express from "express";
+import dotenv from "dotenv";
+import { Request, Response } from "express";
+
+// Configure dotenv
+dotenv.config();
+
+//Utils
+import { connectDB } from "./config/db.js";
+
+connectDB();
+
+const app = express();
+const port = process.env.PORT;
+console.log(port);
+
+// Middleware to parse JSON
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
